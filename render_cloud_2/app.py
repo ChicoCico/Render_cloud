81ec4e5cbbc72bceb26f71a0ce52159c9d0ecdfd
ChicoCico <159574521+ChicoCico@users.noreply.github.com>
2024-02-09 20:47:41 +0200

    Delete render_cloud_2 directory

--- render_cloud_2/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import random #підсключення бібліотеки рандом
-app = Flask(__name__)
-
-@app.route('/')
-def random_n(): # створення функції для виведення випадкових чисел
-    random_number=random.randint(1,6) #виведення випадкових чисел в діапазоні значень
-    return f"Ваше випадкове число = {random_number}" 
-
